b7fa5561c5913dba6b86163534444e7a9c3d3ed0
amrinder <amrinder@10.48.14.71>
2024-01-24 10:21:51 -0500

    Added part time student class

---
+++ src/arrayde/PrtTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrayde;
+
+/**
+ *
+ * @author amrinder
+ */
+public class PrtTimeStudent extends Student {
+    private int numOfCourses;
+
+  
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    public PrtTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
